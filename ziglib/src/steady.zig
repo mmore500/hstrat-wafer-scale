011a3324bc7c8c65b786e186e009fd0c6ecf3244
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-03-26 12:04:17 -0400

    Isolate manuscript source

--- ziglib/src/steady.zig
+++
@@ -1,5 +0,0 @@
-/// How many surface sites are made available to the surface update algorithm?
-pub fn get_num_positions(surfaceSize: i32) i32 {
-    // site 0 on actual surface is excluded
-    return surfaceSize - 1;
-}
